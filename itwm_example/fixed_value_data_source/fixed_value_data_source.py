074cf9348de0d9182a4c0c39f9ee79d9938736db
Stefano Borini <sborini@enthought.com>
2018-05-10 15:39:23 +0100

    Added fixed value data source

---
+++ itwm_example/fixed_value_data_source/fixed_value_data_source.py
@@ -0,0 +1,23 @@
+from force_bdss.api import BaseDataSource, DataValue, Slot
+
+
+class FixedValueDataSource(BaseDataSource):
+    """Defines a data source that returns a fixed value
+    specified in the model.
+    """
+
+    def run(self, model, parameters):
+        return [
+            DataValue(
+                type=model.cuba_type_out,
+                value=model.value
+            )]
+
+    def slots(self, model):
+        return (
+            (
+            ),
+            (
+                Slot(type=model.cuba_type_out),
+            )
+        )
