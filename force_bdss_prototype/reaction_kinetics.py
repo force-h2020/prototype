f7479b1445601208d0a65306cf105fc2adbe16a3
Stefano Borini <sborini@enthought.com>
2018-05-10 13:08:20 +0100

    Refactored methods that are private in nature

--- force_bdss_prototype/reaction_kinetics.py
+++ force_bdss_prototype/reaction_kinetics.py
@@ -2,8 +2,8 @@ import numpy as np
 from scipy.special import factorial
 from .initializer import Initializer
 
-def analytical_solution(A0, B0, P0, S0, C0, k_ps, t):
-    a = alpha(A0, B0, np.sum(k_ps), t)
+def _analytical_solution(A0, B0, P0, S0, C0, k_ps, t):
+    a = _alpha(A0, B0, np.sum(k_ps), t)
     A = A0 - a
     B = B0 - a
     P = P0 + k_ps[0] / np.sum(k_ps) * a
@@ -11,48 +11,48 @@ def analytical_solution(A0, B0, P0, S0, C0, k_ps, t):
     C = C0
     return np.array([A, B, P, S, C])
 
-def sum1(A0, B0, k, t, n=5):
+def _sum1(A0, B0, k, t, n=5):
     exponent = np.arange(1, n + 1, 1)
     denominator = factorial(exponent)
     result = ((B0 - A0) ** (exponent - 1)) * (k * t) ** exponent
     return np.sum(result / denominator)
 
-def sum2(A0, B0, k, t, n=5):
+def _sum2(A0, B0, k, t, n=5):
     exponent = np.arange(2, n + 1, 1)
     denominator = factorial(exponent) / (exponent - 1)
     result = ((B0 - A0) ** (exponent - 2)) * (k * t) ** exponent
     return np.sum(result / denominator)
 
-def sum3(A0, B0, k, t, n=5):
+def _sum3(A0, B0, k, t, n=5):
     exponent = np.arange(1, n + 1, 1)
     denominator = factorial(exponent) / exponent
     result = ((B0 - A0) ** (exponent - 1)) * (k * t) ** (exponent - 1)
     return np.sum(result / denominator)
 
-def alpha(A0, B0, k, t):
+def _alpha(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
     if epsilon > 8e-2:
         multiplier = np.exp((B0 - A0) * k * t)
         result = A0 * B0 * (multiplier - 1) / (B0 * multiplier - A0)
     else:
-        sum1ab = sum1(A0, B0, k, t)
-        sum1ba = sum1(B0, A0, k, t)
+        sum1ab = _sum1(A0, B0, k, t)
+        sum1ba = _sum1(B0, A0, k, t)
         result = (A0 * B0 * sum1ab) / (1. + (B0 * sum1ab))
         result += (A0 * B0 * sum1ba) / (1. + (A0 * sum1ba))
         result /= 2
     return result
 
-def dalda(A0, B0, k, t):
+def _dalda(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
     if epsilon > 8e-2:
         B0expo = B0 * np.exp((B0 - A0) * k * t)
         result = B0expo * (B0expo + k * t * A0**2 - k * t * A0 * B0 - B0)
         result /= (B0expo - A0)**2
     else:
-        sum1ab = sum1(A0, B0, k, t)
-        sum1ba = sum1(B0, A0, k, t)
-        sum2ab = sum2(A0, B0, k, t)
-        sum2ba = sum2(B0, A0, k, t)
+        sum1ab = _sum1(A0, B0, k, t)
+        sum1ba = _sum1(B0, A0, k, t)
+        sum2ab = _sum2(A0, B0, k, t)
+        sum2ba = _sum2(B0, A0, k, t)
         p1 = (B0 * sum1ab - A0 * B0 * sum2ab) * (1 + B0 * sum1ab)
         p2 = (-B0 * sum2ab) * (A0 * B0 * sum1ab)
         p3 = (B0 * sum1ba + A0 * B0 * sum2ba) * (1 + A0 * sum1ba)
@@ -62,7 +62,7 @@ def dalda(A0, B0, k, t):
         result /= 2
     return result
 
-def daldb(A0, B0, k, t):
+def _daldb(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
     if epsilon > 8e-2:
         expo = np.exp((B0 - A0) * k * t)
@@ -70,10 +70,10 @@ def daldb(A0, B0, k, t):
         result = A0 * ((k * t * B0**2 - k * t * A0 * B0 - A0) * expo + A0)
         result /= (B0expo - A0)**2
     else:
-        sum1ab = sum1(A0, B0, k, t)
-        sum1ba = sum1(B0, A0, k, t)
-        sum2ab = sum2(A0, B0, k, t)
-        sum2ba = sum2(B0, A0, k, t)
+        sum1ab = _sum1(A0, B0, k, t)
+        sum1ba = _sum1(B0, A0, k, t)
+        sum2ab = _sum2(A0, B0, k, t)
+        sum2ba = _sum2(B0, A0, k, t)
         p1 = (A0 * sum1ab + A0 * B0 * sum2ab) * (1 + B0 * sum1ab)
         p2 = (sum1ab + B0 * sum2ab) * A0 * B0 * sum1ab
         p3 = (A0 * sum1ba - A0 * B0 * sum2ba) * (1 + A0 * sum1ba)
@@ -83,16 +83,16 @@ def daldb(A0, B0, k, t):
         result /= 2
     return result
 
-def daldk(A0, B0, k, t):
+def _daldk(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
     if epsilon > 8e-2:
         B0expo = B0 * np.exp((B0 - A0) * k * t)
         result = t * A0 * B0expo * (B0 - A0)**2 / (B0expo - A0)**2
     else:
-        sum1ab = sum1(A0, B0, k, t)
-        sum1ba = sum1(B0, A0, k, t)
-        sum3ab = sum3(A0, B0, k, t)
-        sum3ba = sum3(B0, A0, k, t)
+        sum1ab = _sum1(A0, B0, k, t)
+        sum1ba = _sum1(B0, A0, k, t)
+        sum3ab = _sum3(A0, B0, k, t)
+        sum3ba = _sum3(B0, A0, k, t)
         p1 = A0 * B0 * t * sum3ab * (1 + B0 * sum1ab)
         p2 = B0 * t * sum3ab * A0 * B0 * sum1ab
         p3 = A0 * B0 * t * sum3ba * (1 + A0 * sum1ba)
@@ -102,16 +102,16 @@ def daldk(A0, B0, k, t):
         result /= 2
     return result
 
-def daldt(A0, B0, k, t):
+def _daldt(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
     if epsilon > 8e-2:
         B0expo = B0 * np.exp((B0 - A0) * k * t)
         result = k * A0 * B0expo * (B0 - A0)**2 / (B0expo - A0)**2
     else:
-        sum1ab = sum1(A0, B0, k, t)
-        sum1ba = sum1(B0, A0, k, t)
-        sum3ab = sum3(A0, B0, k, t)
-        sum3ba = sum3(B0, A0, k, t)
+        sum1ab = _sum1(A0, B0, k, t)
+        sum1ba = _sum1(B0, A0, k, t)
+        sum3ab = _sum3(A0, B0, k, t)
+        sum3ba = _sum3(B0, A0, k, t)
         p1 = A0 * B0 * k * sum3ab * (1 + B0 * sum1ab)
         p2 = B0 * k * sum3ab * A0 * B0 * sum1ab
         p3 = A0 * B0 * k * sum3ba * (1 + A0 * sum1ba)
@@ -121,17 +121,17 @@ def daldt(A0, B0, k, t):
         result /= 2
     return result
 
-def grad_x(A0, B0, P0, S0, C0, k_ps, t):
+def _grad_x(A0, B0, P0, S0, C0, k_ps, t):
     grad_x_X_mat = np.empty((5, 7))
     kp, ks = k_ps
     k = np.sum(k_ps)
     kpk = kp / k
     ksk = ks / k
-    da = dalda(A0, B0, k, t)
-    db = daldb(A0, B0, k, t)
-    dk = daldk(A0, B0, k, t)
-    dt = daldt(A0, B0, k, t)
-    al = alpha(A0, B0, k, t)
+    da = _dalda(A0, B0, k, t)
+    db = _daldb(A0, B0, k, t)
+    dk = _daldk(A0, B0, k, t)
+    dt = _daldt(A0, B0, k, t)
+    al = _alpha(A0, B0, k, t)
     dada = 1 - da
     dadb = - db
     dadk = - dk
@@ -157,7 +157,7 @@ def grad_x(A0, B0, P0, S0, C0, k_ps, t):
     grad_x_X_mat[4, :] = np.array([0, 0, 0, 0, 1, 0, 0])
     return grad_x_X_mat
 
-def calc_k(T, M):
+def _calc_k(T, M):
     M_v, M_delta_H = M
     R = 8.3144598e-3
     k_ps = M_v * np.exp(-M_delta_H / (R * T))
@@ -177,9 +177,9 @@ class Reaction_kinetics:
     def run(self, X0, M):
         # solver of kinetic module
         R = 8.3144598e-3
-        k_ps = calc_k(X0[5], M)
-        X_mat = analytical_solution(*X0[:5], k_ps, X0[6])
-        grad_x_X_mat = grad_x(*X0[:5], k_ps, X0[6])
+        k_ps = _calc_k(X0[5], M)
+        X_mat = _analytical_solution(*X0[:5], k_ps, X0[6])
+        grad_x_X_mat = _grad_x(*X0[:5], k_ps, X0[6])
         dkdT = 1 / (R * X0[5])**2 * np.sum(k_ps * M[0])
         grad_x_X_mat[:, 5] = dkdT * grad_x_X_mat[:, 5]
         return (X_mat, grad_x_X_mat)
--- force_bdss_prototype/tests/test_reaction_kinetics.py
+++ force_bdss_prototype/tests/test_reaction_kinetics.py
@@ -1,8 +1,8 @@
 import unittest
 import numpy as np
 
-from ..reaction_kinetics import Reaction_kinetics, analytical_solution, \
-                                grad_x, calc_k
+from ..reaction_kinetics import Reaction_kinetics, _analytical_solution, \
+                                _grad_x, _calc_k
 
 A = { "name": "eductA", "manufacturer": "", "pdi": 0 }
 B = { "name": "eductB", "manufacturer": "", "pdi": 0 }
@@ -31,30 +31,30 @@ class Reaction_kineticsTestCase(unittest.TestCase):
 
     def test_analytic_solutionreturn_type(self):
         params = [0.4, 0.5, 0., 0., 0.1, np.array([1, 2]), 20]
-        self.assertEqual(type(analytical_solution(*params)), nptype)
+        self.assertEqual(type(_analytical_solution(*params)), nptype)
 
     def test_analytic_solutionreturn_shape(self):
         params = [0.4, 0.5, 0., 0., 0.1, np.array([1, 2]), 20]
-        self.assertEqual(analytical_solution(*params).shape, (5,))
+        self.assertEqual(_analytical_solution(*params).shape, (5,))
 
     def test_grad_x_return_type(self):
         params = [0.4, 0.5, 0., 0., 0.1, np.array([1, 2]), 20]
-        gradx = grad_x(*params)
+        gradx = _grad_x(*params)
         self.assertEqual(type(gradx), nptype)
 
     def test_grad_x_return_shape(self):
         params = [0.4, 0.5, 0., 0., 0.1, np.array([1, 2]), 20]
-        gradx = grad_x(*params)
+        gradx = _grad_x(*params)
         self.assertEqual(gradx.shape, (5, 7))
 
     def test_calc_k_return_type(self):
-        kps = calc_k(330, M)
+        kps = _calc_k(330, M)
         self.assertEqual(type(kps), nptype)
 
     def test_calc_k_return_shape(self):
-        kps = calc_k(330, M)
+        kps = _calc_k(330, M)
         self.assertEqual(kps.shape, (2,))
 
     def test_calc_k_return(self):
-        kps = calc_k(330, M)
+        kps = _calc_k(330, M)
         self.assertTrue(kps[0] > kps[1])
