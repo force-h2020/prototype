2b543c8c9c519e5af1473b89cf5676e29b61b8e3
Stefano Borini <sborini@enthought.com>
2018-06-05 17:52:44 +0100

    Added default view

--- itwm_example/pure_densities/pure_densities_model.py
+++ itwm_example/pure_densities/pure_densities_model.py
@@ -1,4 +1,5 @@
 from traits.api import Float
+from traitsui.api import View, Item
 
 from force_bdss.api import BaseDataSourceModel
 
@@ -7,3 +8,9 @@ class PureDensitiesModel(BaseDataSourceModel):
     a_pure_density = Float(1.0)
     b_pure_density = Float(1.0)
     c_pure_density = Float(1.0)
+
+    default_traits_view = View(
+        Item("a_pure_density"),
+        Item("b_pure_density"),
+        Item("c_pure_density"),
+    )
