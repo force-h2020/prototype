corrected gradients and objectives

force_bdss_prototype/MCOsolver.py
@@ -22,11 +22,11 @@ class MCOsolver:
         i = 0
         for self.w[0] in np.linspace(0, 1, N):
             for self.w[1] in np.linspace(0, 1 - self.w[0],
-                                         N - round((N - 1)*self.w[0])):
+                                         int(N - round((N - 1)*self.w[0]))):
                 self.w[2] = 1 - self.w[0] - self.w[1]
-                #if not np.any(self.w == 0):
                 i += 1
                 progress(i, (N*N + N)/2)
+                #if not np.any(self.w == 0):
                 self.store_curr_res(self.KKTsolver(new_obj, new_obj_jac))
         return self.res[:self.i]
 
@@ -46,7 +46,6 @@ class MCOsolver:
 def progress(count, total, status=''):
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
-
     percents = round(100.0 * count / float(total), 1)
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))

force_bdss_prototype/MCOwrapper.py
@@ -4,6 +4,7 @@ from .constraints import Constraints
 from .initializer import Initializer
 from .MCOsolver import MCOsolver
 from .pareto_process_db import Pareto_process_db
+from .process_db_access import Process_db_access
 
 
 class MCOwrapper:
@@ -19,6 +20,8 @@ class MCOwrapper:
         obj_f = lambda y: self.obj.obj_calc(y)[0]
         obj_jac = lambda y: self.obj.obj_calc(y)[1]
         X0 = self.ini.get_init_data_kin_model(self.R, self.C)
+        p_db_access = Process_db_access(R)
+        self.C_supplier = p_db_access.get_C_supplier()
         # X0 structure:
         # 0: A concentration
         # 1: 0.5. B concentration
@@ -31,13 +34,14 @@ class MCOwrapper:
         self.mcosolver = MCOsolver(y0, constr, obj_f, obj_jac)
 
     def solve(self):
-        results = self.mcosolver.solve(N=100)
+        results = self.mcosolver.solve(N=50)
         res = np.empty((results.shape[0], results.shape[1] + 3))
         for i in range(results.shape[0]):
             y = results[i, :]
             O, _ = self.obj.obj_calc(y)
             res[i, :results.shape[1]] = results[i, :]
             res[i, results.shape[1]:] = O
+        res[:, results.shape[1]] = self.C_supplier * np.exp(res[:, results.shape[1]])
         self.pp_db = Pareto_process_db(res)
         self.pp_db.write_csv()
         return res

force_bdss_prototype/initializer.py
@@ -40,10 +40,10 @@ class Initializer:
             # Transferred to json
             S = self.react_knowledge.get_side_products(R)[0]
             R_S = { "reactants": R["reactants"], "products": [S] }
-            vp, grad_Hp = self.m_db_access.get_arrhenius_params(R)
-            vs, grad_Hs = self.m_db_access.get_arrhenius_params(R_S)
+            vp, delta_Hp = self.m_db_access.get_arrhenius_params(R)
+            vs, delta_Hs = self.m_db_access.get_arrhenius_params(R_S)
             M_v = np.array([vp, vs])
-            M_delta_H = np.array([grad_Hp, grad_Hs])
+            M_delta_H = np.array([delta_Hp, delta_Hs])
             return (M_v, M_delta_H)
 
     instance = None

force_bdss_prototype/process_db_access.py
@@ -7,12 +7,13 @@ class Process_db_access:
 
         def __init__(self, R):
             self.R = R
-            self.V_r = 1.
-            self.W = 1.
-            self.const_A = 1.
-            self.cost_B = .9
-            self.C_supplier = .1
-            self.cost_purification = 2.
+            self.V_r = 1000.
+            self.W = 1e-8
+            self.const_A = 5e-3
+            self.cost_B = 4e-3
+            self.quad_coeff = 1e-5
+            self.C_supplier = .01 * 5
+            self.cost_purification = 0.5e-3
 
         def get_prod_cost(self, X_proc):
             # Transferred
@@ -25,7 +26,7 @@ class Process_db_access:
         def get_contamination_range(self, A):
             # Transferred to json
             # [C] in mol/l
-            c_min = 0.001
+            c_min = self.C_supplier * 1e-9
             c_max = self.C_supplier
             return (c_min, c_max)
 
@@ -40,6 +41,9 @@ class Process_db_access:
             # Transferred to json
             return self.V_r
 
+        def get_C_supplier(self):
+            return self.C_supplier
+
         def get_mat_cost(self, V_a, C_e, V_b, p_C):
             # Transferred
             # V_a + V_b <= V_r
@@ -47,15 +51,17 @@ class Process_db_access:
             const_A = self.const_A
             cost_purification = self.cost_purification
             cost_B = self.cost_B
-            tot_cost_A = cost_purification * np.log(self.C_supplier / C_e)
+            tot_cost_A = cost_purification * (C_e / self.C_supplier - 1)**2
             tot_cost_A += const_A
             tot_cost_A *= V_a
+            tot_cost_A += V_r * self.quad_coeff * (V_a - 600)**2
             tot_cost_B = V_b * cost_B
             cost = float(tot_cost_A + tot_cost_B)
-            dva = tot_cost_A / V_a - cost_B
-            dce = - V_a * cost_purification /C_e
+            dva = const_A - cost_B + cost_purification * (C_e / self.C_supplier - 1)**2
+            dva += V_r * self.quad_coeff * 2 * (V_a - 600)
+            dce = V_a * cost_purification * (2 * C_e / self.C_supplier**2 - 2 / self.C_supplier)
             grad_y_cost = np.array([dva, dce, 0, 0])
-            return (cost, grad_y_cost)
+            return (cost / 100, grad_y_cost / 100)
 
     instance = None
 

force_bdss_prototype/reaction_kinetics.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy.special import factorial
 from .initializer import Initializer
 
+tol = 4e-1
+
 def _analytical_solution(A0, B0, P0, S0, C0, k_ps, t):
     a = _alpha(A0, B0, np.sum(k_ps), t)
     A = A0 - a
@@ -35,7 +37,7 @@ def _sum3(A0, B0, k, t, n=4):
 
 def _alpha(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
-    if epsilon > 0.4e-2:
+    if epsilon > tol:
         multiplier = np.exp((B0 - A0) * k * t)
         result = A0 * B0 * (multiplier - 1) / (B0 * multiplier - A0)
     else:
@@ -49,7 +51,7 @@ def _alpha(A0, B0, k, t):
 
 def _dalda(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
-    if epsilon > 0.4e-2:
+    if epsilon > tol:
         B0expo = B0 * np.exp((B0 - A0) * k * t)
         result = B0expo * (B0expo + k * t * A0**2 - k * t * A0 * B0 - B0)
         result /= (B0expo - A0)**2
@@ -70,7 +72,7 @@ def _dalda(A0, B0, k, t):
 
 def _daldb(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
-    if epsilon > 0.4e-2:
+    if epsilon > tol:
         expo = np.exp((B0 - A0) * k * t)
         B0expo = B0 * expo
         result = A0 * ((k * t * B0**2 - k * t * A0 * B0 - A0) * expo + A0)
@@ -92,7 +94,7 @@ def _daldb(A0, B0, k, t):
 
 def _daldk(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
-    if epsilon > 0.4e-2:
+    if epsilon > tol:
         B0expo = B0 * np.exp((B0 - A0) * k * t)
         result = t * A0 * B0expo * (B0 - A0)**2 / (B0expo - A0)**2
     else:
@@ -112,7 +114,7 @@ def _daldk(A0, B0, k, t):
 
 def _daldt(A0, B0, k, t):
     epsilon = np.abs((A0 - B0) * k * t)
-    if epsilon > 0.4e-2:
+    if epsilon > tol:
         B0expo = B0 * np.exp((B0 - A0) * k * t)
         result = k * A0 * B0expo * (B0 - A0)**2 / (B0expo - A0)**2
     else:
@@ -154,13 +156,13 @@ def _grad_x(A0, B0, P0, S0, C0, k_ps, t):
     dpda = kpk * da
     dpdb = kpk * db
     dpdp = 1
-    dpdk = ksk / k * al + kpk * dk
+    dpdk = kpk * dk
     dpdt = kpk * dt
     grad_x_X_mat[2, :] = np.array([dpda, dpdb, dpdp, 0, 0, dpdk, dpdt])
     dsda = ksk * da
     dsdb = ksk * db
     dsds = 1
-    dsdk = kpk / k * al + ksk * dk
+    dsdk = ksk * dk
     dsdt = ksk * dt
     grad_x_X_mat[3, :] = np.array([dsda, dsdb, 0, dsds, 0, dsdk, dsdt])
     grad_x_X_mat[4, :] = np.array([0, 0, 0, 0, 1, 0, 0])
@@ -200,6 +202,11 @@ class Reaction_kinetics:
                                X0[4],
                                k_ps,
                                X0[6])
-        dkdT = 1 / (R * X0[5])**2 * np.sum(k_ps * M[0])
+        dkdT = 1 / (R * X0[5]**2) * np.sum(k_ps * M[1])
         grad_x_X_mat[:, 5] = dkdT * grad_x_X_mat[:, 5]
+        dkskdT = (M[1][1] - M[1][0]) * k_ps[0] * k_ps[1]
+        dkskdT /= R * X0[5]**2 * (k_ps[0] * M[0][1] / M[0][0] + k_ps[1] * M[0][0] / M[0][1])**2
+        dkpkdT = - dkskdT
+        grad_x_X_mat[2, 5] += _alpha(X0[0], X0[1], np.sum(k_ps), X0[6]) * dkpkdT
+        grad_x_X_mat[3, 5] += _alpha(X0[0], X0[1], np.sum(k_ps), X0[6]) * dkskdT
         return (X_mat, grad_x_X_mat)