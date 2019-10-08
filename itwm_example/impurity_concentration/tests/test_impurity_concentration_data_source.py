9e584fc9f8285d8d9f9bed27ef7a75a7d5629217
Petr Kungurtsev <pkungurtsev@gmail.com>
2019-10-08 17:52:22 +0100

    data source: slots test

--- itwm_example/impurity_concentration/tests/test_impurity_concentration_data_source.py
+++ itwm_example/impurity_concentration/tests/test_impurity_concentration_data_source.py
@@ -9,6 +9,14 @@ class TestImpurityConcentrationDataSource(unittest.TestCase):
         self.plugin = ExamplePlugin()
         self.factory = self.plugin.data_source_factories[4]
 
+    def test_data_source_slots(self):
+        data_source = self.factory.create_data_source()
+        model = self.factory.create_model()
+
+        input_slots, output_slots = data_source.slots(model)
+        self.assertEqual(12, len(input_slots))
+        self.assertEqual(2, len(output_slots))
+
     def test_basic_evaluation(self):
         data_source = self.factory.create_data_source()
         model = self.factory.create_model()
@@ -26,12 +34,16 @@ class TestImpurityConcentrationDataSource(unittest.TestCase):
             1.0,
             1.0,
         ]
-        in_slots = data_source.slots(model)[0]
+        input_slots, output_slots = data_source.slots(model)
+
+        self.assertEqual(len(values), len(input_slots))
+
         data_values = [
             DataValue(type=slot.type, value=value)
-            for slot, value in zip(in_slots, values)
+            for slot, value in zip(input_slots, values)
         ]
 
         res = data_source.run(model, data_values)
 
+        self.assertEqual(len(output_slots), len(res))
         self.assertAlmostEqual(res[0].value, 0.372027129767, 6)
