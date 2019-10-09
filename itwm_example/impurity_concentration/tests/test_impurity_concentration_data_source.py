d510a20ce77c13887c155ac7c0c70237bf739fed
Petr Kungurtsev <pkungurtsev@gmail.com>
2019-10-09 11:47:07 +0100

    Refactor test with the use of the template class for DataSource

--- itwm_example/impurity_concentration/tests/test_impurity_concentration_data_source.py
+++ itwm_example/impurity_concentration/tests/test_impurity_concentration_data_source.py
@@ -1,25 +1,12 @@
-import unittest
-
-from force_bdss.api import DataValue
-from itwm_example.example_plugin import ExamplePlugin
+from itwm_example.tests.probe_classes.data_source import ProbeDataSource
 
 
-class TestImpurityConcentrationDataSource(unittest.TestCase):
+class TestImpurityConcentrationDataSource(ProbeDataSource):
     def setUp(self):
-        self.plugin = ExamplePlugin()
-        self.factory = self.plugin.data_source_factories[4]
-
-    def test_data_source_slots(self):
-        data_source = self.factory.create_data_source()
-        model = self.factory.create_model()
-
-        input_slots, output_slots = data_source.slots(model)
-        self.assertEqual(12, len(input_slots))
-        self.assertEqual(2, len(output_slots))
+        self.index = 4
+        super().setUp()
 
     def test_basic_evaluation(self):
-        data_source = self.factory.create_data_source()
-        model = self.factory.create_model()
         values = [
             0.5,
             0.1,
@@ -34,16 +21,8 @@ class TestImpurityConcentrationDataSource(unittest.TestCase):
             1.0,
             1.0,
         ]
-        input_slots, output_slots = data_source.slots(model)
+        data_values = self.convert_to_data_values(values, self.input_slots)
 
-        self.assertEqual(len(values), len(input_slots))
+        res = self.data_source.run(self.model, data_values)
 
-        data_values = [
-            DataValue(type=slot.type, value=value)
-            for slot, value in zip(input_slots, values)
-        ]
-
-        res = data_source.run(model, data_values)
-
-        self.assertEqual(len(output_slots), len(res))
         self.assertAlmostEqual(res[0].value, 0.372027129767, 6)
