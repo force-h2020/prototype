b9f1e55f491e4f7804b90953e956d76dc5f95c19
Petr Kungurtsev <pkungurtsev@gmail.com>
2019-10-10 17:37:26 +0100

    Implemented base_test methods for data source signature and basic evaluations

--- itwm_example/tests/probe_classes/data_source.py
+++ itwm_example/tests/probe_classes/data_source.py
@@ -7,6 +7,9 @@ from itwm_example.example_plugin import ExamplePlugin
 
 class BaseTestDataSource(unittest.TestCase):
     _data_source_index = None
+    test_case_values = None
+    test_case_objectives = None
+    _objective_precision = 6
 
     def setUp(self):
         self.plugin = ExamplePlugin()
@@ -24,3 +27,21 @@ class BaseTestDataSource(unittest.TestCase):
             DataValue(type=slot.type, value=value)
             for slot, value in zip(slots, values)
         ]
+
+    def base_test_gradient_type(self):
+        self.assertEqual(
+            self.output_slots[0].type + "_GRADIENT",
+            self.output_slots[1].type
+        )
+
+    def base_test_basic_evaluation(self):
+        for i, values in enumerate(self.test_case_values):
+            data_values = self.convert_to_data_values(values, self.input_slots)
+
+            res = self.data_source.run(self.model, data_values)
+
+            self.assertAlmostEqual(
+                res[0].value,
+                self.test_case_objectives[i],
+                self._objective_precision
+            )
