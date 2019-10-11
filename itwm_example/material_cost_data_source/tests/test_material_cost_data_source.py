58e5f569ef42536ca06bfaf681dc93c5b95f487c
Petr Kungurtsev <pkungurtsev@gmail.com>
2019-10-11 13:21:46 +0100

    MaterialCost data source tests

---
+++ itwm_example/material_cost_data_source/tests/test_material_cost_data_source.py
@@ -0,0 +1,25 @@
+from itwm_example.tests.base_test_classes.base_test_data_source \
+    import BaseTestDataSource
+
+
+class TestMaterialCostDataSource(BaseTestDataSource):
+    def setUp(self):
+        self._data_source_index = 3
+        super().setUp()
+        self.test_case_values = [
+            [1., 1., 1., 1.]
+        ]
+        self.test_case_objectives = [
+            1.,
+        ]
+
+    def test_basic_evaluation(self):
+        super().base_test_basic_evaluation()
+        pass
+
+    def test_gradient_type(self):
+        super().base_test_gradient_type()
+        pass
+
+    def test_param_to_gradient(self):
+        super().base_test_param_to_gradient()
