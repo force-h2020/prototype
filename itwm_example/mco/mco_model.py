33fd38447682a82aaf678fa50e7447d1609a8db0
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-10-17 17:41:40 +0100

    DEV: MCOModel UI placeholder

--- itwm_example/mco/mco_model.py
+++ itwm_example/mco/mco_model.py
@@ -8,8 +8,11 @@ class MCOModel(BaseMCOModel):
 
     evaluation_mode = Enum("Internal", "Subprocess")
 
+    space_search_strategy = Enum("Uniform", "Dirichlet")
+
     def default_traits_view(self):
         return View(
             Item('num_points'),
-            Item('evaluation_mode')
+            Item('evaluation_mode'),
+            Item('space_search_strategy')
         )
