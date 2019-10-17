3b2f47f91b19d9c8c19b9fbbadc3c7f2b3a7715e
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-10-17 16:25:56 +0100

    TST: Tests for the SpaceSamplers

--- itwm_example/mco/space_sampling/tests/test_space_samplers.py
+++ itwm_example/mco/space_sampling/tests/test_space_samplers.py
@@ -1,7 +1,9 @@
 import unittest
 
 from itwm_example.mco.space_sampling.space_samplers import (
+    SpaceSampler,
     DirichletSpaceSampler,
+    UniformSpaceSampler,
     convert_samples_pp_to_samples_total
 )
 
@@ -16,7 +18,19 @@ class TestSamplesConvert(unittest.TestCase):
         self.assertEqual(samples_total, known_samples_total)
 
 
-class TestDirichletSpaceSampler(unittest.TestCase):
+class BaseTestSampler(unittest.TestCase):
+    distribution = SpaceSampler
+
+    def generate_values(self, *args, **kwargs):
+        return self.distribution(
+            *args,
+            **kwargs
+        ).generate_space_sample(**kwargs)
+
+
+class TestDirichletSpaceSampler(BaseTestSampler):
+    distribution = DirichletSpaceSampler
+
     def setUp(self):
         self.dimensions = [3, 1, 5]
         self.alphas = [1, 0.5, 10]
@@ -59,3 +73,100 @@ class TestDirichletSpaceSampler(unittest.TestCase):
                         sum(sample),
                         1.
                     )
+
+
+class TestUniformSpaceSampler(BaseTestSampler):
+    distribution = UniformSpaceSampler
+
+    def setUp(self):
+        self.sampler = self.distribution(
+            dimension=3,
+            resolution=5
+        )
+
+    def test_space_sample(self):
+        self.assertEqual(
+            list(self.generate_values(3, 5)),
+            [
+                [1.0, 0.0, 0.0],
+                [0.75, 0.25, 0.0],
+                [0.75, 0.0, 0.25],
+                [0.50, 0.50, 0.0],
+                [0.50, 0.25, 0.25],
+                [0.50, 0.0, 0.50],
+                [0.25, 0.75, 0.0],
+                [0.25, 0.50, 0.25],
+                [0.25, 0.25, 0.50],
+                [0.25, 0.0, 0.75],
+                [0.0, 1.0, 0.0],
+                [0.0, 0.75, 0.25],
+                [0.0, 0.50, 0.50],
+                [0.0, 0.25, 0.75],
+                [0.0, 0.0, 1.0],
+            ]
+        )
+
+        self.assertEqual(
+            list(self.generate_values(1, 5)),
+            [[1.0]]
+        )
+        self.assertEqual(list(self.generate_values(2, 5)), [
+            [1.0, 0.0],
+            [0.75, 0.25],
+            [0.50, 0.50],
+            [0.25, 0.75],
+            [0.0, 1.0],
+        ])
+
+        self.assertEqual(
+            list(self.generate_values(3, 9)),
+            [[1.0, 0.0, 0.0], [0.875, 0.125, 0.0], [0.875, 0.0, 0.125],
+             [0.75, 0.25, 0.0], [0.75, 0.125, 0.125], [0.75, 0.0, 0.25],
+             [0.625, 0.375, 0.0], [0.625, 0.25, 0.125], [0.625, 0.125, 0.25],
+             [0.625, 0.0, 0.375], [0.5, 0.5, 0.0], [0.5, 0.375, 0.125],
+             [0.5, 0.25, 0.25], [0.5, 0.125, 0.375], [0.5, 0.0, 0.5],
+             [0.375, 0.625, 0.0], [0.375, 0.5, 0.125], [0.375, 0.375, 0.25],
+             [0.375, 0.25, 0.375], [0.375, 0.125, 0.5], [0.375, 0.0, 0.625],
+             [0.25, 0.75, 0.0], [0.25, 0.625, 0.125], [0.25, 0.5, 0.25],
+             [0.25, 0.375, 0.375], [0.25, 0.25, 0.5], [0.25, 0.125, 0.625],
+             [0.25, 0.0, 0.75], [0.125, 0.875, 0.0], [0.125, 0.75, 0.125],
+             [0.125, 0.625, 0.25], [0.125, 0.5, 0.375], [0.125, 0.375, 0.5],
+             [0.125, 0.25, 0.625], [0.125, 0.125, 0.75], [0.125, 0.0, 0.875],
+             [0.0, 1.0, 0.0], [0.0, 0.875, 0.125], [0.0, 0.75, 0.25],
+             [0.0, 0.625, 0.375], [0.0, 0.5, 0.5], [0.0, 0.375, 0.625],
+             [0.0, 0.25, 0.75], [0.0, 0.125, 0.875], [0.0, 0.0, 1.0]]
+        )
+
+        self.assertEqual(
+            list(self.generate_values(2, 5, zero_values=False)),
+            [
+                [0.75, 0.25],
+                [0.50, 0.50],
+                [0.25, 0.75],
+            ]
+        )
+
+        self.assertEqual(
+            list(self.generate_values(3, 9, zero_values=False)),
+            [[0.75, 0.125, 0.125],
+             [0.625, 0.25, 0.125],
+             [0.625, 0.125, 0.25],
+             [0.5, 0.375, 0.125],
+             [0.5, 0.25, 0.25],
+             [0.5, 0.125, 0.375],
+             [0.375, 0.5, 0.125],
+             [0.375, 0.375, 0.25],
+             [0.375, 0.25, 0.375],
+             [0.375, 0.125, 0.5],
+             [0.25, 0.625, 0.125],
+             [0.25, 0.5, 0.25],
+             [0.25, 0.375, 0.375],
+             [0.25, 0.25, 0.5],
+             [0.25, 0.125, 0.625],
+             [0.125, 0.75, 0.125],
+             [0.125, 0.625, 0.25],
+             [0.125, 0.5, 0.375],
+             [0.125, 0.375, 0.5],
+             [0.125, 0.25, 0.625],
+             [0.125, 0.125, 0.75]]
+        )
