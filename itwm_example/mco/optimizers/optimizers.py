a884fce91d5d00a61418f237e625e69efa9e9948
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-10-21 16:21:48 +0100

    MAINT: MockOptimizer extended to handle the test_kpi_scaling case

--- itwm_example/mco/optimizers/optimizers.py
+++ itwm_example/mco/optimizers/optimizers.py
@@ -89,5 +89,10 @@ class MockOptimizer:
         self.margins = np.array([10.0 for _ in range(self.dimension)])
         self.min_values = np.array([i for i in range(self.dimension)])
 
+        self.scaling_values = np.array([0.1] * self.dimension)
+
     def optimize(self):
         return 0, self.min_values + self.weights * self.margins
+
+    def weighted_optimize(self, weights):
+        return 0, self.min_values + weights * self.margins
