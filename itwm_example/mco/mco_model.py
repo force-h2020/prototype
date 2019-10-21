c4d747858dba82403ccf00797f56bbf3f2c03ec9
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-10-21 13:48:12 +0100

    MAINT: Small refactor of the MCO model methods for space samples generation; docstring added

--- itwm_example/mco/mco_model.py
+++ itwm_example/mco/mco_model.py
@@ -17,20 +17,26 @@ class MCOModel(BaseMCOModel):
 
     def default_traits_view(self):
         return View(
-            Item('num_points'),
-            Item('evaluation_mode'),
-            Item('space_search_strategy')
+            Item("num_points"),
+            Item("evaluation_mode"),
+            Item("space_search_strategy"),
         )
 
-    def weights_samples(self, **kwargs):
+    def _space_search_distribution(self, **kwargs):
+        """ Generates space search distribution object, based on
+        the user settings of the `space_search_strategy` trait."""
+
         if self.space_search_strategy == "Uniform":
             distribution = UniformSpaceSampler
         elif self.space_search_strategy == "Dirichlet":
             distribution = DirichletSpaceSampler
         else:
             raise NotImplementedError
-        return distribution(
-            len(self.kpis),
-            self.num_points,
+        return distribution(len(self.kpis), self.num_points, **kwargs)
+
+    def weights_samples(self, **kwargs):
+        """ Generates necessary number of search space sample points
+        from the internal search strategy."""
+        return self._space_search_distribution(
             **kwargs
         ).generate_space_sample()
