82330d81a76ddaf1f2a83da9984678db87ee3ae5
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-12-02 13:51:40 +0000

    MAINT: moved WeightedOptimizer specific methods from MCO to inside the optimizer class

--- itwm_example/mco/mco.py
+++ itwm_example/mco/mco.py
@@ -1,18 +1,14 @@
 import logging
 import sys
 
-import numpy as np
-
-from traits.api import Type
+from traits.api import Enum
 
 from force_bdss.api import BaseMCO, DataValue
 
 from .subprocess_workflow_evaluator import SubprocessWorkflowEvaluator
-from .scaling_tools.kpi_scaling import sen_scaling_method
-from .optimizers.optimizers import IOptimizer, WeightedOptimizer, NevergradOptimizer
-from .space_sampling.space_samplers import (
-    UniformSpaceSampler,
-    DirichletSpaceSampler,
+from .optimizers.optimizers import (
+    WeightedOptimizer,
+    NevergradOptimizer,
 )
 
 log = logging.getLogger(__name__)
@@ -20,76 +16,14 @@ log = logging.getLogger(__name__)
 
 class MCO(BaseMCO):
 
-    optimizer = Type(IOptimizer)
-
-    scaling_method = staticmethod(sen_scaling_method)
+    optimizer = Enum(WeightedOptimizer, NevergradOptimizer)
 
     def _optimizer_default(self):
         return WeightedOptimizer
 
-    def get_scaling_factors(self, optimizer, kpis, scaling_method=None):
-        """Calculates scaling factors for KPIs, defined in MCO.
-        Scaling factors are calculated (as required) by the provided scaling
-        method. In general, this provides normalization values for the possible
-        range of each KPI..
-        Performs scaling for all KPIs that have `auto_scale == True`.
-        Otherwise, keeps the default scale factor.
-
-        Parameters
-        ----------
-        optimizer: IOptimizer instance
-            Instance that provides optimization functionality
-        kpis: List[KPISpecification]
-            List of KPI objects to scale
-        scaling_method: callable
-            A method to scale KPI weights. Default set to the Sen's
-            "Multi-Objective Programming Method"
-        """
-        if scaling_method is None:
-            scaling_method = self.scaling_method
-
-        #: Get default scaling weights for each KPI variable
-        default_scaling_factors = np.array([kpi.scale_factor for kpi in kpis])
-
-        #: Apply a wrapper for the evaluator weights assignment and
-        #: call of the .optimize method.
-        #: Then, calculate scaling factors defined by the `scaling_method`
-        scaling_factors = scaling_method(len(kpis), optimizer.optimize)
-
-        #: Apply the scaling factors where necessary
-        auto_scales = [kpi.auto_scale for kpi in kpis]
-        default_scaling_factors[auto_scales] = scaling_factors[auto_scales]
-
-        log.info(
-            "Using KPI scaling factors: {}".format(default_scaling_factors)
-        )
-
-        return default_scaling_factors.tolist()
-
-    @staticmethod
-    def _space_search_distribution(model, **kwargs):
-        """ Generates space search distribution object, based on
-        the user settings of the `space_search_strategy` trait."""
-
-        if model.space_search_mode == "Uniform":
-            distribution = UniformSpaceSampler
-        elif model.space_search_mode == "Dirichlet":
-            distribution = DirichletSpaceSampler
-        else:
-            raise NotImplementedError
-        return distribution(len(model.kpis), model.num_points, **kwargs)
-
-    def weights_samples(self, model, **kwargs):
-        """ Generates necessary number of search space sample points
-        from the internal search strategy."""
-        return self._space_search_distribution(
-            model, **kwargs
-        ).generate_space_sample()
-
     def run(self, evaluator):
 
         model = evaluator.mco_model
-        kpis = model.kpis
 
         if model.evaluation_mode == "Subprocess":
             # Here we create an instance of our WorkflowEvaluator subclass
@@ -106,18 +40,11 @@ class MCO(BaseMCO):
 
         optimizer = self.optimizer(evaluator, model)
 
-        #: Get scaling factors and non-zero weight combinations for each KPI
-        scaling_factors = self.get_scaling_factors(optimizer, kpis)
-        for weights in self.weights_samples(model):
-
-            log.info("Doing MCO run with weights: {}".format(weights))
-
-            scaled_weights = [
-                weight * scale
-                for weight, scale in zip(weights, scaling_factors)
-            ]
-
-            optimal_point, optimal_kpis = optimizer.optimize(scaled_weights)
+        for (
+            optimal_point,
+            optimal_kpis,
+            scaled_weights,
+        ) in optimizer.optimize():
 
             # When there is new data, this operation informs the system that
             # new data has been received. It must be a dictionary as given.
@@ -125,13 +52,4 @@ class MCO(BaseMCO):
                 [DataValue(value=v) for v in optimal_point],
                 [DataValue(value=v) for v in optimal_kpis],
                 scaled_weights,
-            )
-
-        self.optimizer = NevergradOptimizer
-        optimizer = self.optimizer(evaluator, model)
-        for optimal_point, optimal_kpis in optimizer.optimize():
-            self.notify_new_point(
-                [DataValue(value=v) for v in optimal_point],
-                [DataValue(value=v) for v in optimal_kpis],
-                [1, 1, 1],
             )
--- itwm_example/mco/optimizers/optimizers.py
+++ itwm_example/mco/optimizers/optimizers.py
@@ -6,12 +6,22 @@ from scipy import optimize as scipy_optimize
 import nevergrad as ng
 from nevergrad.functions import MultiobjectiveFunction
 
-from traits.api import Interface, HasStrictTraits, provides, List, Instance
+from traits.api import (
+    Interface,
+    HasTraits,
+    HasStrictTraits,
+    provides,
+    Instance,
+)
 
-from force_bdss.api import BaseMCOParameter
 from force_bdss.mco.i_evaluator import IEvaluator
 
 from itwm_example.mco.mco_model import MCOModel
+from itwm_example.mco.scaling_tools.kpi_scaling import sen_scaling_method
+from itwm_example.mco.space_sampling.space_samplers import (
+    UniformSpaceSampler,
+    DirichletSpaceSampler,
+)
 
 log = logging.getLogger(__name__)
 
@@ -25,7 +35,7 @@ class IOptimizer(Interface):
 
 
 @provides(IOptimizer)
-class WeightedOptimizer(HasStrictTraits):
+class WeightedOptimizer(HasTraits):
     """Performs a scipy optimise with SLSQP method given a set of weights
     for the individual KPIs.
     """
@@ -33,6 +43,8 @@ class WeightedOptimizer(HasStrictTraits):
     single_point_evaluator = Instance(IEvaluator)
 
     model = Instance(MCOModel)
+
+    scaling_method = staticmethod(sen_scaling_method)
 
     def __init__(self, single_point_evaluator, model):
         super().__init__(
@@ -47,8 +59,80 @@ class WeightedOptimizer(HasStrictTraits):
 
         return score
 
-    def optimize(self, weights):
-        return self._weighted_optimize(weights)
+    def get_scaling_factors(self, scaling_method=None):
+        """ Calculates scaling factors for KPIs, defined in MCO.
+        Scaling factors are calculated (as required) by the provided scaling
+        method. In general, this provides normalization values for the possible
+        range of each KPI.
+        Performs scaling for all KPIs that have `auto_scale == True`.
+        Otherwise, keeps the default scale factor.
+
+        Parameters
+        ----------
+        scaling_method: callable
+            A method to scale KPI weights. Default set to the Sen's
+            "Multi-Objective Programming Method"
+        """
+        if scaling_method is None:
+            scaling_method = self.scaling_method
+
+        kpis = self.model.kpis
+
+        #: Get default scaling weights for each KPI variable
+        default_scaling_factors = np.array([kpi.scale_factor for kpi in kpis])
+
+        #: Apply a wrapper for the evaluator weights assignment and
+        #: call of the .optimize method.
+        #: Then, calculate scaling factors defined by the `scaling_method`
+        scaling_factors = scaling_method(len(kpis), self._weighted_optimize)
+
+        #: Apply the scaling factors where necessary
+        auto_scales = [kpi.auto_scale for kpi in kpis]
+        default_scaling_factors[auto_scales] = scaling_factors[auto_scales]
+
+        log.info(
+            "Using KPI scaling factors: {}".format(default_scaling_factors)
+        )
+
+        return default_scaling_factors.tolist()
+
+    def _space_search_distribution(self, **kwargs):
+        """ Generates space search distribution object, based on
+        the user settings of the `space_search_strategy` trait."""
+
+        if self.model.space_search_mode == "Uniform":
+            distribution = UniformSpaceSampler
+        elif self.model.space_search_mode == "Dirichlet":
+            distribution = DirichletSpaceSampler
+        else:
+            raise NotImplementedError
+        return distribution(
+            len(self.model.kpis), self.model.num_points, **kwargs
+        )
+
+    def weights_samples(self, **kwargs):
+        """ Generates necessary number of search space sample points
+        from the internal search strategy."""
+        return self._space_search_distribution(
+            **kwargs
+        ).generate_space_sample()
+
+    def optimize(self):
+        #: Get scaling factors and non-zero weight combinations for each KPI
+        scaling_factors = self.get_scaling_factors()
+        for weights in self.weights_samples():
+
+            log.info("Doing MCO run with weights: {}".format(weights))
+
+            scaled_weights = [
+                weight * scale
+                for weight, scale in zip(weights, scaling_factors)
+            ]
+
+            optimal_point, optimal_kpis = self._weighted_optimize(
+                scaled_weights
+            )
+            yield optimal_point, optimal_kpis, scaled_weights
 
     def _weighted_optimize(self, weights):
         initial_point = [p.initial_value for p in self.model.parameters]
