68ddfdbc0f95a73a89f1ecc3497d09923a3a18df
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-12-04 14:49:17 +0000

    TST: MCOModel tests for optimizer create / update, get state

--- itwm_example/mco/tests/test_mco.py
+++ itwm_example/mco/tests/test_mco.py
@@ -8,6 +8,10 @@ from force_bdss.api import (
 )
 
 from itwm_example.mco.mco_factory import MCOFactory
+from itwm_example.mco.optimizers.optimizers import (
+    WeightedOptimizer,
+    NevergradOptimizer,
+)
 
 
 class TestMCO(TestCase):
@@ -46,6 +50,48 @@ class TestMCO(TestCase):
             self.mco_model.optimizer._weighted_optimize([0.5, 0.5])
             self.assertEqual(7, mock_exec.call_count)
 
-    def test_change_optimizer(self):
+    def test_default_optimizer(self):
+        self.assertIsInstance(self.mco_model.optimizer, WeightedOptimizer)
+
+    def test_update_optimizer(self):
         self.mco_model.optimizer_mode = "NeverGrad"
+        self.assertIs(
+            self.mco_model._optimizer_from_mode(), NevergradOptimizer
+        )
+        self.assertIsInstance(self.mco_model.optimizer, NevergradOptimizer)
+        self.assertEqual(self.mco_model.optimizer.algorithms, "TwoPointsDE")
         self.mco_model.optimizer_mode = "Weighted"
+        self.assertIs(self.mco_model._optimizer_from_mode(), WeightedOptimizer)
+        self.assertIsInstance(self.mco_model.optimizer, WeightedOptimizer)
+        self.assertEqual(self.mco_model.optimizer.algorithms, "SLSQP")
+
+    def test_update_kpis(self):
+        new_kpis = [
+            KPISpecification(name="new"),
+            KPISpecification(name="another_new"),
+        ]
+        self.mco_model.kpis = new_kpis
+        self.assertEqual(self.mco_model.kpis, new_kpis)
+        self.assertEqual(self.mco_model.optimizer.kpis, new_kpis)
+
+    def test_update_parameters(self):
+        new_parameters = [2, 2, 2]
+        new_parameters = [
+            self.factory.parameter_factories[0].create_model()
+            for _ in new_parameters
+        ]
+        self.mco_model.parameters = new_parameters
+        self.assertEqual(self.mco_model.parameters, new_parameters)
+        self.assertEqual(self.mco_model.optimizer.parameters, new_parameters)
+
+    def test___getstate__(self):
+        state = self.mco_model.__getstate__()
+        self.assertEqual("Weighted", state["optimizer_mode"])
+        self.assertEqual("Weighted_Optimizer", state["optimizer_data"]["name"])
+        self.assertEqual("SLSQP", state["optimizer_data"]["algorithms"])
+
+        self.mco_model.optimizer_mode = "NeverGrad"
+        state = self.mco_model.__getstate__()
+        self.assertEqual("NeverGrad", state["optimizer_mode"])
+        self.assertEqual("Nevergrad", state["optimizer_data"]["name"])
+        self.assertEqual("TwoPointsDE", state["optimizer_data"]["algorithms"])
