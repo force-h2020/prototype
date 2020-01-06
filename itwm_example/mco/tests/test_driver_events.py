a5c3c9e6bad73441c4a250725cc8c79a7a664c78
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-01-06 14:46:42 +0000

    DEV: Added custom MCOModel start event

---
+++ itwm_example/mco/tests/test_driver_events.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from itwm_example.mco.driver_enents import ITWMMCOStartEvent
+
+class TestProgressEvents(TestCase):
+    def test_getstate_start_event(self):
+        event = ITWMMCOStartEvent(
+            parameter_names=["p1", "p2", "p3"], kpi_names=["kpi"]
+        )
+        self.assertDictEqual(
+            event.__getstate__(),
+            {"parameter_names": ["p1", "p2", "p3"], "kpi_names": ["kpi"]},
+        )
+        self.assertEqual(
+            ["p1", "p2", "p3", "kpi", "kpi weight"], event.serialize()
+        )
