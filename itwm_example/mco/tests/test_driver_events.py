a195d3ba29e05cbba97f7e7a9b441e911474efc4
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-01-06 15:08:22 +0000

    STY: flake8

--- itwm_example/mco/tests/test_driver_events.py
+++ itwm_example/mco/tests/test_driver_events.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from itwm_example.mco.driver_enents import ITWMMCOStartEvent
 
+
 class TestProgressEvents(TestCase):
     def test_getstate_start_event(self):
         event = ITWMMCOStartEvent(
