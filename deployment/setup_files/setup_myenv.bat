7ccacd9e7f5ebb6c13f72e88c511053cc5355b8b
Burgardmforce <burgardm@itwm.fraunhofer.de>
2020-03-04 22:14:31 +0100

    unnecessary path removed

--- deployment/setup_files/setup_myenv.bat
+++ deployment/setup_files/setup_myenv.bat
@@ -2,5 +2,4 @@ cd /D %UserProfile%
 py -3 -m pip install --upgrade virtualenv
 virtualenv myenv
 cmd /k "myenv\Scripts\activate.bat & pip install --upgrade cython numpy matplotlib scipy sympy pip wheel setuptools docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew & pip install --upgrade kivy.deps.gstreamer & pip install --upgrade kivy & pip install --upgrade kivy-garden & garden install --upgrade graph & myenv\Scripts\deactivate.bat & EXIT"
-cd Desktop\FORCE-BDSS-Prototype\force-bdss-plugin-itwm-example\deployment\setup_files
-PAUSE
+EXIT
